9ff27f5c771dd2dae9333db7ed5c6fd9a5853179
Daniel Sloof <d.sloof@drecomm.nl>
2011-11-10 18:07:54 +0100

    At the very least we want to index price and catalog<>product relation.

--- code/Model/Import/Entity/Product.php
+++ code/Model/Import/Entity/Product.php
@@ -32,4 +32,39 @@ class Danslo_ApiImport_Model_Import_Entity_Product extends Mage_ImportExport_Mod
             ->_initCustomerGroups();
     }
     
+    protected function _indexEntities() {
+        /*
+         * Run some of the indexers for newly imported entities.
+         */
+        $entities = array();
+        foreach($this->_newSku as $sku) {
+            $entities[] = $sku['entity_id'];
+        }
+        
+        /*
+         * Set up transport object containing entities.
+         */
+        $transport = Mage::getModel('index/event');
+        $transport->setNewData(array(
+            'product_ids'               => $entities,   // for category_indexer_product
+            'reindex_price_product_ids' => $entities    // for product_indexer_price
+        ));
+
+        /*
+         * TODO: Some error handling.
+         */
+        Mage::getResourceSingleton('catalog/category_indexer_product')->catalogProductMassAction($transport);
+        Mage::getResourceSingleton('catalog/product_indexer_price')->catalogProductMassAction($transport);
+
+        return $this;
+    }
+    
+    public function _importData() {
+        $result = parent::_importData();
+        if($result) {
+            $this->_indexEntities();
+        }
+        return $result;
+    }
+    
 }
