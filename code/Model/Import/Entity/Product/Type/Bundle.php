cc137ace2f9c8d6f2caa6ac42c7dc114961a3cd6
Daniel Sloof <d.sloof@drecomm.nl>
2011-12-05 11:13:34 +0100

    Adding stubs for bundle product support.

---
+++ code/Model/Import/Entity/Product/Type/Bundle.php
@@ -0,0 +1,13 @@
+<?php
+
+class Danslo_ApiImport_Model_Import_Entity_Product_Type_Bundle
+    extends Mage_ImportExport_Model_Import_Entity_Product_Type_Abstract {
+
+    public function saveData() {
+        /*
+         * TODO: Actually implement saving bundles.
+         */
+        return false;
+    }
+
+}
