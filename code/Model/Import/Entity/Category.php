0795dde16f57904ff338ae994c89118dada6f078
Daniel Sloof <d.sloof@drecomm.nl>
2012-05-14 11:18:37 +0200

    Add some phpdoc stuff.

--- code/Model/Import/Entity/Category.php
+++ code/Model/Import/Entity/Category.php
@@ -19,35 +19,87 @@ class Danslo_ApiImport_Model_Import_Entity_Category
     extends Mage_ImportExport_Model_Import_Entity_Abstract
 {
 
-    protected $_categories = array();
-    protected $_defaultAttributeSetId;
-
+    /**
+     * Permanent column names.
+     */
     const COL_NAME   = 'name';
     const COL_PARENT = 'parent';
 
+    /**
+     * Error codes.
+     */
     const ERROR_INVALID_PARENT_CATEGORY = 'invalidParentCategory';
 
+    /**
+     * Default attributeset ID for categories.
+     *
+     * @var int
+     */
+    protected $_defaultAttributeSetId;
+
+    /**
+     * Existing category entities.
+     *
+     * @var array
+     */
+    protected $_categories = array();
+
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
         $this->_dataSourceModel = Danslo_ApiImport_Model_Import::getDataSourceModel();
         $this->_initCategories()
-             ->_initAttributeSet();
+             ->_initDefaultAttributeSetId();
     }
 
+    /**
+     * Create category entity from raw data.
+     *
+     * @throws Exception
+     * @return bool Result of operation.
+     */
     protected function _importData()
     {
-        return $this->_saveCategories();
+        if (Mage_ImportExport_Model_Import::BEHAVIOR_DELETE == $this->getBehavior()) {
+            $this->_deleteCategories();
+        } else {
+            $this->_saveCategories();
+        }
+        Mage::dispatchEvent('catalog_category_import_finish_before', array('adapter' => $this));
+        return true;
     }
 
-    protected function _initAttributeSet()
+    /**
+     * Delete categories.
+     *
+     * @return Danslo_ApiImport_Model_Import_Entity_Category
+     */
+    protected function _deleteCategories()
+    {
+        return $this;
+    }
+
+    /**
+     * Initialize the default attributeset ID for category entities.
+     *
+     * @return Danslo_ApiImport_Model_Import_Entity_Category
+     */
+    protected function _initDefaultAttributeSetId()
     {
         $entityType = Mage::getSingleton('eav/config')->getEntityType($this->getEntityTypeCode());
         $this->_defaultAttributeSetId = $entityType->getDefaultAttributeSetId();
+        return $this;
     }
 
-    /*
-     * Implementation is similar to product, but we start one level up.
+    /**
+     * Initialize categories text-path starting from root level.
+     *
+     * @return Danslo_ApiImport_Model_Import_Entity_Category
      */
     protected function _initCategories()
     {
@@ -66,6 +118,11 @@ class Danslo_ApiImport_Model_Import_Entity_Category
         return $this;
     }
 
+    /**
+     * Gather and save information about category entities.
+     *
+     * @return Danslo_ApiImport_Model_Import_Entity_Category
+     */
     protected function _saveCategories()
     {
         while ($bunch = $this->_dataSourceModel->getNextBunch()) {
@@ -110,6 +167,14 @@ class Danslo_ApiImport_Model_Import_Entity_Category
         return $this;
     }
 
+    /**
+     * Update and insert data in entity table.
+     *
+     * @param array $entityRowsIn Row for insert
+     * @param array $entityRowsUp Row for update
+     *
+     * @return Danslo_ApiImport_Model_Import_Entity_Category
+     */
     protected function _saveCategoryEntity(array $entityRowsIn, array $entityRowsUp)
     {
         static $entityTable = null;
@@ -133,11 +198,24 @@ class Danslo_ApiImport_Model_Import_Entity_Category
         return $this;
     }
 
+    /**
+     * EAV entity type code getter.
+     *
+     * @abstract
+     * @return string
+     */
     public function getEntityTypeCode()
     {
         return 'catalog_category';
     }
 
+    /**
+     * Check for existence of a parent category.
+     *
+     * @param array $rowData
+     * @param int $rowNum
+     * @return bool
+     */
     protected function _isParentCategoryValid($rowData, $rowNum)
     {
         if (!isset($this->_categories[$rowData[self::COL_PARENT]])) {
@@ -147,8 +225,19 @@ class Danslo_ApiImport_Model_Import_Entity_Category
         return true;
     }
 
+    /**
+     * Validate data row.
+     *
+     * @param array $rowData
+     * @param int $rowNum
+     * @return boolean
+     */
     public function validateRow(array $rowData, $rowNum)
     {
+        /**
+         * TODO: Right now, parent category must be an existing entity.
+         * We should build a hierarchy of to-be-imported entities so we can import everything at the same time.
+         */
         $this->_isParentCategoryValid($rowData, $rowNum);
         return !isset($this->_invalidRows[$rowNum]);
     }
