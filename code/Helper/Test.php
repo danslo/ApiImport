61247a33c7f049abd2ccf8a67cdb876e0f9c79cd
willy-ahva <willy.mesnage@akeneo.com>
2014-09-24 16:41:24 +0200

    Refactor and split code
    Fix bug with called api method
    Fix bug with group order
    Remove groupToUpdate of _updateAttributeSetsAndGroups
    Split importAttributeSet in attribute sets and attribute associations

--- code/Helper/Test.php
+++ code/Helper/Test.php
@@ -96,7 +96,8 @@ class Danslo_ApiImport_Helper_Test
         'General',
         'Prices',
         'Marketing',
-        'Color'
+        'Color',
+        'Size'
     );
 
     /**
@@ -160,12 +161,13 @@ class Danslo_ApiImport_Helper_Test
 
         for ($i = 1; $i <= $numProducts; $i++) {
             $attributeSets[$i] = array(
-                'name'      => 'set ' . $i,
-                'sortOrder' => $i,
+                'attribute_set_name'       => 'set ' . $i,
+                'sortOrder'                => $i,
                 $this->_attributeGroups[0] => 1,
                 $this->_attributeGroups[1] => 2,
-                $this->_attributeGroups[2] => 4,
-                $this->_attributeGroups[3] => 3
+                $this->_attributeGroups[2] => 3,
+                $this->_attributeGroups[3] => 4,
+                $this->_attributeGroups[4] => 5
             );
         }
 
@@ -184,12 +186,11 @@ class Danslo_ApiImport_Helper_Test
 
         for ($i = 1; $i <= $numProducts; $i++) {
             $type  = $this->_attributeTypes[array_rand($this->_attributeTypes)];
-            $group = $this->_attributeGroups[array_rand($this->_attributeGroups)];
 
             $attributes[$i] = array(
-                'attribute_code'          => 'attr_test_' . $i,
+                'attribute_id'            => 'attr_test_' . $i,
                 'type'                    => $type,
-                'default_value_' . $type  => 'Default value for attribute test ' . $i,
+                'default'                 => 'Default value of the DOOM for attribute test ' . $i,
                 'label'                   => 'My Attribute test ' . $i,
                 'input'                   => 'text',
                 'user_defined'            => true,
@@ -203,10 +204,38 @@ class Danslo_ApiImport_Helper_Test
                 'is_filterable_in_search' => false,
                 'used_for_sort_by'        => true,
                 'used_in_product_listing' => true,
-                'comparable'              => true,
-                'attribute_set_id'        => 'set ' . rand(1, 10),
-                'group'                   => $group
+                'comparable'              => true
             );
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * Generates random attribute association
+     *
+     * @param  int   $numProducts
+     * @return array
+     */
+    public function generateRandomStandardAttributeAssociations($numAssoc, $numAttrPerSet = 2)
+    {
+        $attributes = array();
+        $numSets = floor($numAssoc / $numAttrPerSet);
+
+        $it = 1;
+        for ($i = 1; $i <= $numSets; $i++) {
+            for ($j = 1; $j <= $numAttrPerSet; $j++) {
+                $group = $this->_attributeGroups[array_rand($this->_attributeGroups)];
+
+                $attributes[$it] = array (
+                    'attribute_id'       => 'attr_test_' . rand(1, $numAssoc),
+                    'attribute_set_id'   => 'set ' . $i,
+                    'attribute_group_id' => $group,
+                    'sort_order'         => rand(0, $numAttrPerSet)
+                );
+
+                $it++;
+            }
         }
 
         return $attributes;
