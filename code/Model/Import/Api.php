5a73216af17b1203db33767f52ede98dba904eb5
Sébastien BEAU <sebastien.beau@akretion.com>
2014-10-19 23:49:24 +0200

    throw error in the web API if we encountered errors during the import.

--- code/Model/Import/Api.php
+++ code/Model/Import/Api.php
@@ -65,6 +65,11 @@ class Danslo_ApiImport_Model_Import_Api
         $this->_api->getDataSourceModel()->setEntities($entities);
         try {
             $result = $this->_api->importSource();
+            $errorsCount = $this->_api->getErrorsCount();
+            if ($errorsCount > 0) {
+                Mage::throwException("There were {$errorsCount} errors during the import process." .
+                    "Please be aware that valid entities were still imported.");
+            };
         } catch(Mage_Core_Exception $e) {
             $this->_fault('import_failed', $e->getMessage());
         }
