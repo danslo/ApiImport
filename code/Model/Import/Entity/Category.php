6e951af0fab70653a450505d7623d9419da73272
Daniel Sloof <goapsychadelic@gmail.com>
2015-02-11 18:39:12 +0100

    fix the licensing on category import

--- code/Model/Import/Entity/Category.php
+++ code/Model/Import/Entity/Category.php
@@ -1,27 +1,11 @@
 <?php
 /**
- * Magento
+ * Entity Adapter for importing Magento Categories
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_ImportExport
- * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @category   Danslo
+ * @package    Danslo_ApiImport
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software Licence 3.0 (OSL-3.0)
+ * @author     Paul Hachmang <paul@h-o.nl>
  */
 
 class Danslo_ApiImport_Model_Import_Entity_Category
