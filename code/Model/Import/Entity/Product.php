203095ac7251c9da12e1c38d1513cb96166534ab
Elias Kotlyar <elias.kotlyar@gmail.com>
2015-10-17 11:44:09 -0700

    Added Patch for Category-Mapping with Category-Id instead of Category Path

--- code/Model/Import/Entity/Product.php
+++ code/Model/Import/Entity/Product.php
@@ -294,5 +294,38 @@ class Danslo_ApiImport_Model_Import_Entity_Product
         }
         return $this;
     }
+    /**
+     * Initialize categories text-path to ID hash.
+     * Also adds a CategoryID-Mapping
+     *
+     * @return Mage_ImportExport_Model_Import_Entity_Product
+     */
+    protected function _initCategories()
+    {
+        $collection = Mage::getResourceModel('catalog/category_collection')->addNameToResult();
+        /* @var $collection Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection */
+        foreach ($collection as $category) {
+            $structure = explode('/', $category->getPath());
+            $pathSize  = count($structure);
+            if ($pathSize > 1) {
+                $path = array();
+                for ($i = 1; $i < $pathSize; $i++) {
+                    $path[] = $collection->getItemById($structure[$i])->getName();
+                }
+                $rootCategoryName = array_shift($path);
+                if (!isset($this->_categoriesWithRoots[$rootCategoryName])) {
+                    $this->_categoriesWithRoots[$rootCategoryName] = array();
+                }
+                $index = implode('/', $path);
+                $this->_categoriesWithRoots[$rootCategoryName][$index] = $category->getId();
+                if ($pathSize > 2) {
+                    $this->_categories[$index] = $category->getId();
+                }
+                $this->_categories[$category->getId()] = $category->getId();
+            }
+        }
+        return $this;
+    }
+
 
 }
